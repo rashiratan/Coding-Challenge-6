4023823ce82ed1f83431f20cbcb52725ee979782
Rashi Ratan <rashiiratan@gmail.com>
2024-09-22 13:00:19 -0400

    Initialize employees with shift schedules

---
+++ employee_shift_management.js
@@ -0,0 +1,9 @@
+//Task 1:
+
+const employees = [
+    {name: 'Taylor Swift', shifts: [{ day: 'Monday', hours: 7 }, { day: 'Wednesday', hours: 4 }] },
+    {name: 'Charlie Puth', shifts: [{ day: 'Tuesday', hours: 9 }, { day: 'Thursday', hours: 3 }] },
+    {name: 'Ed Sheeran', shifts: [{ day: 'Friday', hours: 6 }, { day: 'Monday', hours: 4 }] },
+    {name: 'Selena Gomez', shifts: [{ day: 'Thursday', hours: 8 }] }
+];
+//creating an array of employee objects that stores their shift details
