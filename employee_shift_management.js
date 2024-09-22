b9830d28ffdf26caf98ca1f4ef89ec5a38646787
Rashi Ratan <rashiiratan@gmail.com>
2024-09-22 13:03:13 -0400

    Create assignShift function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -15,3 +15,26 @@ function displayEmployeeShifts(employeeShift) {
         console.log(`day: ${shift.day}, hours: ${shift.hours}`)); 
     //using forEach and template literals to display details of the employee object input
 }
+
+//Task 3
+function assignShift(employeeName, day, hours){
+    let employeeFound = employees.find((employee) => employee.name === employeeName); //finding employee in employees array
+    let shiftExists;
+    if (!employeeFound) {
+        return `Sorry, employee not found.`;
+        //logging error if name does not match an employee in the array
+        }
+    else{
+        shiftExists = employeeFound.shifts.find((shift) => shift.day === day);
+        //find a match in employee's shifts
+        if (shiftExists){
+        return `Sorry, ${employeeName} is already working ${shiftExists.hours} hours on this day.`;
+        //logging error if they are already working that day
+    }
+    else {
+        employeeFound.shifts.push({day: day, hours: hours});
+        //pushing the employee's new shift to the array
+        return `Thanks! New shift has been added.`;
+        //logging update to user
+    }}
+}
